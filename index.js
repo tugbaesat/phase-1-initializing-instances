e271b1f73db994919b740341df8c12b9e839899c
Tugba Esat <tugbaesat007@gmail.com>
2023-05-26 15:01:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad;
+        this.drink = drink;
+        this.soup = soup;
+    }
+}
+
+class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad;
+        this.entree = entree;
+        this.soup = soup;
+        this.#dessert = dessert;
+    }
+}
+
